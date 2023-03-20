e2e44b5374c1e2390b7230666ffbe667e1a3d4e0
AULA 204-14 <lunafabianemmanuel@gmail.com>
2023-03-20 20:21:56 -0300

    servidor web local

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("hola mundo");
+//console.log("hola mundo");
+
+const express = require('express');
+const app = express();
+const PORT = 3001;
+app.get('/api', (req,res)=>{
+
+    console.log('paso por aca');
+    res.json({mensaje:'hola mundo'}).status(200)
+
+
+});
+app.listen(PORT,()=>{console.log('estoy escuchando en el puerto: '+PORT)});
